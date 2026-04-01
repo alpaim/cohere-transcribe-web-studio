cfcaacc47d1dc3f313719469731715e4646f7e98
alpaim <156456776+alpaim@users.noreply.github.com>
2026-04-01 19:16:28 +0300

    chore: eslint and path aliases

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import antfu from "@antfu/eslint-config";
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default antfu({
+    ignores: ["bun.lock", ".agents"],
+    stylistic: {
+        semi: true,
+        indent: 4,
+        quotes: "double",
     },
-  },
-])
+    react: true,
+    typescript: {
+        overrides: {
+            "no-console": "off", // not sure if I should keep it for further development iterations
+        },
+    },
+    rules: {
+        "node/prefer-global/process": "off",
+    },
+});
